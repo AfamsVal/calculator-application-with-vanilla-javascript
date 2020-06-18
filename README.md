a7a4d0453938fc12d0d5283c226e434d6cd7ae80
afams val <myceet1@gmail.com>
2020-06-18 09:10:25 +0100

    my simple calculator

---
+++ README.md
@@ -0,0 +1,13 @@
+## MY CALCULATOR APP
+
+> This application is designed using HTML, CSS & vanilla JavaScript.
+
+---
+
+### SET UP
+
+- No special setup is required, kindly download and run index.html in your browser.
+
+![No Image Found](https://laffout.com/images/calculator.jpg)
+
+#### copyright @ Afams Val
---
+++ main.js
@@ -0,0 +1,74 @@
+const getHistory = () => document.querySelector("#history-value").innerText;
+
+const printHistory = (num) =>
+  (document.querySelector("#history-value").innerText = num);
+
+const getOutput = (num) => document.querySelector("#output-value").innerText;
+
+const printOutput = (num) => {
+  if (num == "") {
+    document.getElementById("output-value").innerText = num;
+  } else {
+    document.getElementById("output-value").innerText = getFormatedNumber(num);
+  }
+};
+
+const getFormatedNumber = (num) => {
+  if (num == "-") return "";
+  const n = Number(num);
+  let value = n.toLocaleString("en");
+  return value;
+};
+const reverseNumberFormat = (num) => {
+  return Number(num.replace(/,/g, ""));
+};
+const operator = document.getElementsByClassName("operator");
+let output;
+for (let i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", function () {
+    if (this.id == "clear") {
+      printHistory("");
+      printOutput("");
+    } else if (this.id == "backspace") {
+      output = reverseNumberFormat(getOutput()).toString();
+      if (output) {
+        //if output has a value
+        output = output.substr(0, output.length - 1);
+        printOutput(output);
+      }
+    } else {
+      output = getOutput();
+      let history = getHistory();
+      if (output == "" && history != "") {
+        if (isNaN(history[history.length - 1])) {
+          history = history.substr(0, history.length - 1);
+        }
+      }
+      if (output != "" || history != "") {
+        output = output == "" ? output : reverseNumberFormat(output);
+        history = history + output;
+        if (this.id == "=") {
+          const result = eval(history);
+          printOutput(result);
+          printHistory("");
+        } else {
+          history = history + this.id;
+          printHistory(history);
+          printOutput("");
+        }
+      }
+    }
+  });
+}
+
+let number = document.getElementsByClassName("number");
+for (let i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", function () {
+    let output = reverseNumberFormat(getOutput());
+    if (output != NaN) {
+      //If output is not a number
+      output = output + this.id;
+      printOutput(output);
+    }
+  });
+}
